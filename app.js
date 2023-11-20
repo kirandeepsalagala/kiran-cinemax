e66bac2878faa257e8ad512374ef741720d0c7b5
Kiran Deep <kiran.shine3@gmail.com>
2023-11-20 14:18:06 +0530

    hello

--- app.js
+++ app.js
@@ -45,3 +45,96 @@ function validateLoginDetails () {
         console.log("invalid");
     }
 }
+
+
+
+// function createAndAppendNowPlaying(data){
+//     var rootEl = document.getElementById("root");
+
+//     let nowPlayingRootEl = document.createElement("div");
+//     nowPlayingRootEl.classList.add("d-flex", "flex-row", "justify-content-center");
+//     rootEl.appendChild(nowPlayingRootEl);
+
+//     let moviePosterEl = document.createElement("img");
+//     // moviePosterEl.src = `https://image.tmdb.org/t/p/w500/${data.results[0].poster_path}.jpg`;
+//     moviePosterEl.src = `https://image.tmdb.org/t/p/original/${data.results[0].poster_path}`;
+//     moviePosterEl.classList.add("movie-poster-image");
+//     // console.log(data);
+//     // moviePosterEl.style.color = "white";
+//     nowPlayingRootEl.appendChild(moviePosterEl);
+
+//     let posterPaths = document.createElement("img");
+//     posterPaths.src = `https://image.tmdb.org/t/p/w500/${data.results.map(movie => movie.poster_path)}`;
+//     nowPlayingRootEl.appendChild(posterPaths);
+//     console.log(posterPaths);
+
+//     // const movies = data.results[0,1,2,3,4].poster_path;
+//     // movies.forEach(movies => {
+
+//     //   const imageUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`; // Assuming 'poster_path' contains the image path
+  
+//     //   const imgElement = document.createElement('img');
+//     //   imgElement.src = imageUrl;
+//     //   imgElement.alt = movies.title;
+//     //   nowPlayingRootEl.appendChild(imgElement);
+//     // });
+
+//     // let titleEl = document.createElement("h1");
+//     // titleEl.textContent = data.results.map(movie => movie.title);
+//     // titleEl.style.color = "white";
+//     // nowPlayingRootEl.appendChild(titleEl);
+//     // console.log(titleEl);
+
+// }
+
+
+function nowPlaying () {
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjk1N2Y5NTZiMzhkNGUwOTNiZWRkNDY2ODgxMTQzMSIsInN1YiI6IjY0YmY4NDllMDU4MjI0MDBiMDc2NmQwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UO9XwHD2qpZiDp-_SSWERUnrW39GGyd4mv2ax7wXJlg'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+        // .then(response => response.json())
+        // .then(response => console.log(data))
+        // .catch(err => console.error(err));
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            // createAndAppendNowPlaying(data);
+            // var rootEl = document.getElementById("root");
+
+            // const posterPathEl = document.createElement("h1");
+            // posterPathEl.textContent = data.results.map(movie => movie.title);
+            // rootEl.appendChild(posterPathEl);
+            // console.log(posterPathEl);
+
+            const rootEl = document.getElementById('root');
+
+            let nowPlayingRootEl = document.createElement("div");
+            nowPlayingRootEl.classList.add("d-flex", "flex-row", "poster-overflow");
+            rootEl.appendChild(nowPlayingRootEl);
+
+            if (data.results && data.results.length > 0) {
+              data.results.forEach(movie => {
+                if (movie.poster_path) {
+                  const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+                  const img = document.createElement('img');
+                  img.src = posterUrl;
+                  img.alt = movie.title + ' Poster';
+                  img.classList.add('movie-poster-image');
+                  nowPlayingRootEl.appendChild(img);
+                }
+              });
+            } else {
+              nowPlayingRootEl.textContent = 'No movies found';
+            }
+        })
+}
+
+nowPlaying();
